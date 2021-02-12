46723756306d66aec20c350aa8a69f7e88449926
Duma Roberto Zelaya Mejia <usis007420@ugb.edu.sv>
2021-02-12 13:32:39 -0600

    Conversor de Unidades

--- app/src/main/java/com/example/calculadora1/MainActivity.java
+++ app/src/main/java/com/example/calculadora1/MainActivity.java
@@ -15,57 +15,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void calcular(View view){
-        TextView tempVal = (TextView)findViewById(R.id.txtnum1);
-        double  num1 = Double.parseDouble(tempVal.getText().toString());
 
-        tempVal = (TextView)findViewById(R.id.txtnum2);
-        double num2 = Double.parseDouble(tempVal.getText().toString());
-
-        double respuesta = 1;
-
-        Spinner cboopbasicos = findViewById(R.id.cboOperacionesBasicas);
-        switch (cboopbasicos.getSelectedItemPosition()){
-            case 0:
-                respuesta = num1 + num2;
-                break;
-            case 1:
-                respuesta = num1 - num2;
-                break;
-            case 2:
-                respuesta = num1 * num2;
-                break;
-            case 3:
-                respuesta = num1 / num2;
-                break;
-            case 4:
-                for (int i=2; i<=num1; i++){
-                    respuesta *=i;
-                }
-                break;
-            case 5:
-                respuesta = ((num1 / num2) * 100);
-                break;
-            case 6:
-                respuesta = Math.pow(num1, num2);
-                break;
-            case 7:
-                Double raiz = Math.pow(num1, 1/num2);
-                respuesta = raiz;
-                break;
-            case 8:
-                respuesta = num1 % num2;
-                break;
-            case 9:
-                if (num1 > num2){
-                    respuesta = num1;
-                }
-                else{
-                    respuesta = num2;
-                }
-                break;
-        }
-        tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("respuesta: " + respuesta);
     }
-}
