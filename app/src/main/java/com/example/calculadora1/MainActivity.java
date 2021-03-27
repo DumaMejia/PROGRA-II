ae8ae40022e5d17162d3ee85df4d97bd3b87104f
Duma Roberto Zelaya Mejia <usis007420@ugb.edu.sv>
2021-03-27 13:00:04 -0600

    TAREA 28 DE MARZO CASI TERMINADA

--- app/src/main/java/com/example/calculadora1/MainActivity.java
+++ app/src/main/java/com/example/calculadora1/MainActivity.java
@@ -2,101 +2,45 @@ package com.example.calculadora1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    TabHost tbhConversores;
-    Button btnConvertir;
-    TextView tempVal;
-    Spinner spnOpcionDe, spnOpcionA;
-    conversores miConversor = new conversores();
+    FloatingActionButton btn;
+    DB miBD;
+    ListView ltsAmigos;
+    Cursor datosAmigosCursor = null;
+    ArrayList<amigos> amigosArrayList=new ArrayList<amigos>();
+    ArrayList<amigos> amigosArrayListCopy=new ArrayList<amigos>();
+    amigos misAmigos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tbhConversores = findViewById(R.id.tbhConversores);
-        tbhConversores.setup();
-
-        tbhConversores.addTab(tbhConversores.newTabSpec("Area").setContent(R.id.tabArea).setIndicator("AREA"));
-        tbhConversores.addTab(tbhConversores.newTabSpec("Agua").setContent(R.id.tabAgua).setIndicator("AGUA"));
-
-        btnConvertir = findViewById(R.id.btnCalcular);
-        btnConvertir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    tempVal = (TextView) findViewById(R.id.txtcantidad);
-                    double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                    spnOpcionDe = findViewById(R.id.cboDe);
-                    spnOpcionA = findViewById(R.id.cboA);
-                    tempVal = findViewById(R.id.lblRespuesta);
-                    tempVal.setText("Respuesta: " + miConversor.convertir(0, spnOpcionDe.getSelectedItemPosition(), spnOpcionA.getSelectedItemPosition(), cantidad));
-                }catch (Exception e){
-                    tempVal = findViewById(R.id.lblRespuesta);
-                    tempVal.setText("Por favor ingrese los valores correspondiente");
-                    Toast.makeText(getApplicationContext(), "Por ingrese los valores correspondiente "+ e.getMessage(),Toast.LENGTH_SHORT).show();
-                    /*Snackbar snackbar = Snackbar.make(contenidoView, "Por favor ingrese los valores correspondiente", Snackbar.LENGTH_LONG);
-                    snackbar.show();*/
-                }
-            }
+        btn = findViewById(R.id.btnAgregarAmigos);
+        btn.setOnClickListener(v->{
+            agregarAmigos("nuevo", new String[]{});
         });
-        btnConvertir = findViewById(R.id.btnCalcularAgua);
-        btnConvertir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    tempVal = findViewById(R.id.txtCantidadAgua);
-                    double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                    tempVal = findViewById(R.id.lblRespuestaAgua);
-
-                    tempVal.setText("Respuesta: " + miConversor.convertiragua(cantidad));
-                }catch (Exception e){
-                    tempVal = findViewById(R.id.lblRespuestaAgua);
-                    tempVal.setText("Por favor ingrese los valores correspondiente");
-                    Toast.makeText(getApplicationContext(), "Por ingrese los valores correspondiente "+ e.getMessage(),Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        obtenerDatosAmigos();
+        buscarAmigos();
 
     }
 
     }
 
-class conversores{
-    double[][] conversor = {
-            {1,0.00367, 0.111111,0.092903,0.00014774656489,0.000013292827545,9.2903e-6},
-    };
-    public double convertir(int opcion, int de, int a, double cantidad){
-        return conversor[opcion][a] / conversor[opcion][de] * cantidad;
-    }
-    public double convertiragua( double cantidad){
-        double respuesta = 0;
-        double var1 = 0;
-        double var2 = 0;
 
-        if (cantidad <= 18) {
-            respuesta = 6;
-        }
-        if (cantidad > 18 && cantidad <= 28){
-            respuesta = (cantidad - 18) * 0.45 + 6;
-        }
-        if (cantidad > 28){
-            var1 = (cantidad - 28) * 0.65;
-            var2 = (28-18) * 0.45;
-            respuesta = var1 + var2 + 6;
-        }
-        return respuesta;
-    }
-
-}
